0305164948b28e2f6e7dd52e1e6fa19950b892f5
dilshanSankalpa <dilshansankalpa98@gmail.com>
2022-07-15 20:21:57 +0530

    new version was added

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+guessingGame(){
+  numFiles=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "please enter your guess";
+        read  guess
+        if [ $guess -lt $numFiles ] 
+        then
+            echo "Guess was too low";
+        elif [ $guess -gt $numFiles ]
+        then
+            echo "Guess was too high";
+        else
+            echo "congratulation !, Your are correct .";
+            break;
+        fi
+    done
+
+}
 
 echo "How many files are in the directory?";
-numFiles=$(ls -l |grep "^-"|wc -l)
-echo $numFiles
-while true;
-do
-    echo "please enter your guess";
-    read  guess
-    if [ $guess -lt $numFiles ] 
-    then
-        echo "Guess was too low";
-    elif [ $guess -gt $numFiles ]
-    then
-        echo "Guess was too high";
-    else
-        echo "congratulation !, guess was correctly guessed .";
-        break;
-    fi
-done
+guessingGame
